39c8c8a8accf98dbd5acd890c493afd4c93b92f8
Eduardo Lozas <128528098+LaloLozas16041@users.noreply.github.com>
2023-10-03 17:56:15 -0700

    Update
    NA

--- 02. Classification/02.- K-Nearest Neighbors/20231003_KNN.R
+++ 02. Classification/02.- K-Nearest Neighbors/20231003_KNN.R
@@ -1,14 +1,14 @@
-# Logistic Regression
+# K-Nearest Neighbors (K-NN)
 
 # Importing the dataset
-dataset = read.csv('02. Classification/01. Logistic Regression/Social_Network_Ads.csv')
+dataset = read.csv('Social_Network_Ads.csv')
 dataset = dataset[3:5]
 
 # Encoding the target feature as factor
 dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
 
 # Splitting the dataset into the Training set and Test set
-install.packages('caTools')
+# install.packages('caTools')
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.75)
@@ -19,30 +19,27 @@ test_set = subset(dataset, split == FALSE)
 training_set[-3] = scale(training_set[-3])
 test_set[-3] = scale(test_set[-3])
 
-# Fitting Logistic Regression to the Training set
-classifier = glm(formula = Purchased ~ .,
-                 family = binomial,
-                 data = training_set)
-
-# Predicting the Test set results
-prob_pred = predict(classifier, type = 'response', newdata = test_set[-3])
-y_pred = ifelse(prob_pred > 0.5, 1, 0)
+# Fitting K-NN to the Training set and Predicting the Test set results
+library(class)
+y_pred = knn(train = training_set[, -3],
+             test = test_set[, -3],
+             cl = training_set[, 3],
+             k = 5,
+             prob = TRUE)
 
 # Making the Confusion Matrix
-cm = table(test_set[, 3], y_pred > 0.5)
+cm = table(test_set[, 3], y_pred)
 
 # Visualising the Training set results
-#install.packages("ElemStatLearn")
 library(ElemStatLearn)
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+y_grid = knn(train = training_set[, -3], test = grid_set, cl = training_set[, 3], k = 5)
 plot(set[, -3],
-     main = 'Logistic Regression (Training set)',
+     main = 'K-NN (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -56,10 +53,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+y_grid = knn(train = training_set[, -3], test = grid_set, cl = training_set[, 3], k = 5)
 plot(set[, -3],
-     main = 'Logistic Regression (Test set)',
+     main = 'K-NN (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)

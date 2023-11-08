f90b0fd914e43edde1b82d7a000352f4a4253824
Eduardo Lozas <128528098+LaloLozas16041@users.noreply.github.com>
2023-11-08 10:04:13 -0600

    Update
    Update

--- 01. Regression/01. Simple Linear Regression/20231002_SimpleLinearRegression.r
+++ 01. Regression/01. Simple Linear Regression/20231002_SimpleLinearRegression.r
@@ -1,48 +1,45 @@
-# Simple Linear Regression
+# Regresión lineal simple
 
-# Importing the dataset
+# Importando el conjunto de datos
 dataset = read.csv('01. Regression/01. Simple Linear Regression/Salary_Data.csv')
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
+# Dividir el conjunto de datos en conjunto de entrenamiento y conjunto de prueba
+#install.packages('caTools')
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
+# Escalado de características
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting Simple Linear Regression to the Training set
+# Ajuste de regresión lineal simple al Conjunto de Entrenamiento
 regressor = lm(formula = Salary ~ YearsExperience,
                data = training_set)
 
-# Predicting the Test set results
+# Predicción de los resultados del Conjunto de Test
 y_pred = predict(regressor, newdata = test_set)
 
-# Visualising the Training set results
+# Visualizando los resultados del Conjunto de Entrenamiento
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
              colour = 'red') +
   geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
             colour = 'blue') +
-  ggtitle('Salary vs Experience (Training set)') +
-  xlab('Years of experience') +
-  ylab('Salary')
+  ggtitle('Salario vs Años de Experiencia (Training Set)') +
+  xlab('Años de Experiencia') +
+  ylab('Salario')
 
-# Visualising the Test set results
+# Visualizando los resultados del Conjunto de Test
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
              colour = 'red') +
   geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
             colour = 'blue') +
-  ggtitle('Salary vs Experience (Test set)') +
-  xlab('Years of experience') +
-  ylab('Salary')
-
-
-
+  ggtitle('Salario vs Años de Experiencia (Test Set)') +
+  xlab('Años de Experiencia') +
+  ylab('Salario')
